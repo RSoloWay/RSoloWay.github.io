5286566ec11eb57ecd31ca4e9a28a4d9c0fee65a
RSoloWay <50213677+RSoloWay@users.noreply.github.com>
2019-09-26 03:10:34 +0300

    Add files via upload

---
+++ precache-manifest.6e15385c673cfbc71213e55e27b1965d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba0d36e602723e5ebdd24806e123364",
+    "url": "/index.html"
+  },
+  {
+    "revision": "328ff47aa91bad223902",
+    "url": "/static/css/main.b6e85ba4.chunk.css"
+  },
+  {
+    "revision": "da870d4099106eea9d14",
+    "url": "/static/js/2.5b48ef8a.chunk.js"
+  },
+  {
+    "revision": "328ff47aa91bad223902",
+    "url": "/static/js/main.3f3481f3.chunk.js"
+  },
+  {
+    "revision": "dc01d03442e79eceb953",
+    "url": "/static/js/runtime-main.835341ae.js"
+  },
+  {
+    "revision": "f66915943575942eb68b26aa0c4c1b10",
+    "url": "/static/media/main_bg.f6691594.jpg"
+  },
+  {
+    "revision": "f592f9a37c68ad4f71397030560c8932",
+    "url": "/static/media/thermometer.f592f9a3.svg"
+  }
+]);
