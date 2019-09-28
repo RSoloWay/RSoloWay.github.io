0415e28e3b6afd8c9729cdb9a483e1790f902072
RSoloWay <50213677+RSoloWay@users.noreply.github.com>
2019-09-28 16:11:42 +0300

    Add files via upload

---
+++ precache-manifest.1a5de6f375fa784ba46a20255cdba591.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d785ff37d86cd2ffc0b2e1c7a3088c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18785e63f37655a1cd2f",
+    "url": "/static/css/main.ad51d994.chunk.css"
+  },
+  {
+    "revision": "e9304e508420f18e9a0f",
+    "url": "/static/js/2.ff925b16.chunk.js"
+  },
+  {
+    "revision": "18785e63f37655a1cd2f",
+    "url": "/static/js/main.8253ec90.chunk.js"
+  },
+  {
+    "revision": "fa0d0ea561579e2271aa",
+    "url": "/static/js/runtime-main.f4891672.js"
+  },
+  {
+    "revision": "0a57ad213cb8a584538ae33bacae2edf",
+    "url": "/static/media/main_bg04.0a57ad21.jpg"
+  },
+  {
+    "revision": "f592f9a37c68ad4f71397030560c8932",
+    "url": "/static/media/thermometer.f592f9a3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d522605930eb7f02f3239610d8b34f10.js"
+  "/precache-manifest.1a5de6f375fa784ba46a20255cdba591.js"
 );
 
 self.addEventListener('message', (event) => {
